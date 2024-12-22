c783c05a8d0f7474c5c22e4a6ae531aa4289c2d5
L1ght <2504082957@qq.com>
2024-12-22 17:40:09 +0800

    add ds

---
+++ fzy/README.md
@@ -0,0 +1,5 @@
+## Downloads
+```bash
+export HF_ENDPOINT=https://hf-mirror.com
+huggingface-cli download --resume-download LanguageBind/LanguageBind_Video_FT --local-dir LanguageBind/LanguageBind_Video_FT
+```
---
+++ fzy/ds.py
@@ -0,0 +1,65 @@
+import json
+import jsonlines
+from torch.utils.data import Dataset, DataLoader
+import os
+from PIL import Image
+import torchvision.transforms as transforms
+from pathlib import Path
+PROJECT_BASE = Path(__file__).resolve().parents[1]
+DATASET_BASE = PROJECT_BASE / 'dataset'
+
+class VideoDS(Dataset):
+    def __init__(self, name):
+        self.name = name
+        self.data = list()
+    
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+class ActivityNet(VideoDS):
+    def __init__(self, db_path = DATASET_BASE / 'ActivityNetCaps'):
+        name = 'ActivityNet'
+        super().__init__(name)
+        self.db_path = db_path
+        self.train = json.load(open(db_path / 'train.json'))
+        self.val1 = json.load(open(db_path / 'val_1.json'))
+        self.val2 = json.load(open(db_path / 'val_2.json'))
+        print(f'ActivityNet {name} loaded')
+
+class Breakfast(VideoDS):
+    def __init__(self, db_path = DATASET_BASE / 'breakfast'):
+        name = 'Breakfast'
+        super().__init__(name)
+        self.db_path = db_path
+
+class Charades(VideoDS):
+    def __init__(self, db_path = DATASET_BASE / 'Charades'):
+        name = 'Charades'
+        super().__init__(name)
+        self.db_path = db_path
+
+class QVHighlights(VideoDS):
+    def __init__(self, db_path = DATASET_BASE / 'QVHighlights'):
+        name = 'QVHighlights'
+        super().__init__(name)
+        self.db_path = db_path
+        annotations = self.db_path / 'annotations'
+        self.test = self.load_jsonl(annotations / 'highlight_test_release.jsonl')
+        self.train = self.load_jsonl(annotations / 'highlight_train_release.jsonl')
+        self.val = self.load_jsonl(annotations / 'highlight_val_release.jsonl')
+        
+    def load_jsonl(self, path):
+        data = list()
+        with jsonlines.open(path) as reader:
+            for obj in reader:
+                data.append(obj)
+        return data
+
+     
+
+if __name__ == "__main__":
+    pass
