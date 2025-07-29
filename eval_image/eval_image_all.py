22d63c5ba146087e5e11daaa1e3b3af1b0393cbb
L1ght <2504082957@qq.com>
2025-07-29 20:56:20 +0800

    chore: init eval_image_all

---
+++ eval_image/eval_image_all.py
@@ -0,0 +1,16 @@
+import multiprocessing
+import argparse
+
+# add argument: model_path
+
+
+def run_all(args):
+    # run eval_docvqa_textvqa_chartqa.py --model_path  xxx 
+    # run eval_mme.py --model_path 
+    # run eval_ocrbenchv2.py --model_path
+    
+
+
+if __name__ == "__main__":
+    args = xxx
+    run_all(args)
