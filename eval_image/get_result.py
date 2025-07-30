5874194de83d0462fdc64be90b88876cffbf5a3c
L1ght <2504082957@qq.com>
2025-07-30 15:31:41 +0800

    fix: ocr pipeline

--- eval_image/get_result.py
+++ eval_image/get_result.py
@@ -11,6 +11,46 @@ def write_json(path, outputs):
         json.dump(outputs, f, ensure_ascii=False, indent=4)
     print('Done')
 
+def process_predictions(data_names, predict_model, predict_path=None):
+    """
+    Process OCR predictions and merge with raw data
+    
+    Args:
+        data_names (list): List of data names to process
+        predict_model (str): Model name for prediction
+        predict_path (str, optional): Path to prediction file. If None, uses default logic
+    
+    Returns:
+        list: List of processed outputs with predictions merged
+    """
+    outputs = []
+    
+    for data_name in data_names:
+        # raw_path = f'OCRBench_v2/parsing_{data_name}.json'
+        raw_path = f'OCRBench_v2/OCRBench_v2.json'
+        raw_data = read_json(raw_path)
+        
+        # Use provided predict_path if available, otherwise use default logic
+        if predict_path:
+            current_predict_path = predict_path
+        else:
+            if predict_model == 'onellm':
+                current_predict_path = f'onellm/eval_ocrbench/{data_name}.json'
+            else:
+                current_predict_path = f'eagle_ocr/0726/{data_name}.json'
+        
+        predict_data = read_json(current_predict_path)
+        
+        for raw, predict in zip(raw_data, predict_data):
+            output = raw
+            if predict_model == 'onellm':
+                output['predict'] = predict['predict']
+            else:
+                output['predict'] = predict['prediction'][0]
+            outputs.append(output)
+    
+    return outputs
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process OCR predictions and merge with raw data')
     parser.add_argument('--predict_path', type=str, default=None, 
@@ -26,37 +66,15 @@ if __name__ == '__main__':
     # data_names = ['vqa']
     # data_names = ['vqa', 'doc', 'table', 'formula']
     data_names = ['all_bbox']
-    outputs = []
-    predict_model = args.predict_model
     
     # Use command line argument for output_path if provided, otherwise use default
     if args.output_path:
         output_path = args.output_path
     else:
-        output_path = f'./MultimodalOCR-main/OCRBench_v2/pred_folder/vqa_{predict_model}.json'
+        output_path = f'./MultimodalOCR-main/OCRBench_v2/pred_folder/vqa_{args.predict_model}.json'
     
-    for data_name in data_names:
-        # raw_path = f'OCRBench_v2/parsing_{data_name}.json'
-        raw_path = f'OCRBench_v2/OCRBench_v2.json'
-        raw_data = read_json(raw_path)
-        
-        # Use command line argument for predict_path if provided, otherwise use default logic
-        if args.predict_path:
-            predict_path = args.predict_path
-        else:
-            if predict_model == 'onellm':
-                predict_path = f'onellm/eval_ocrbench/{data_name}.json'
-            else:
-                predict_path = f'eagle_ocr/0726/{data_name}.json'
-        
-        predict_data = read_json(predict_path)
-        for raw, predict in zip(raw_data, predict_data):
-            output = raw
-            if predict_model == 'onellm':
-                output['predict'] = predict['predict']
-            else:
-                output['predict'] = predict['prediction'][0]
-            outputs.append(output)
+    # Process predictions using the extracted function
+    outputs = process_predictions(data_names, args.predict_model, args.predict_path)
 
     write_json(output_path, outputs)
     print(f'Output saved to {output_path}')
