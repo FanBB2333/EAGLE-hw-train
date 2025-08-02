d1d394a098022821772577f901ab439d6ab2a43a
L1ght <2504082957@qq.com>
2025-08-02 11:15:36 +0800

    fix: process data

--- npu/process_dota.py
+++ npu/process_dota.py
@@ -26,6 +26,18 @@ DOTA_CLEAN_DIR = DATA_BASE / "DoTA_CLEAN"
 #     "num_frames": 111,
 #     "subset": "train"
 #   },
+
+sample_format_lambda = lambda question, answer, image_path, id, path_prefix="videos": {
+    'id': str(id),
+    'conversations': [
+        {'from': 'human', 'value': question},
+        {'from': 'gpt', 'value': answer}
+    ],
+    'image': image_path, # /home1/hxl/disk/EAGLE/qbs/Eagle_LanguageBind/dataset/Video/added/ActivityNetCaps/v1-3/train_val/v_ehGHCYKzyZ8.mp4
+    # 'image': f"{path_prefix}/{image_path.split('/')[-1]}" # v_ehGHCYKzyZ8.mp4
+}
+
+
 class DotaDataset(Dataset):
     def __init__(self, metadata_file):
         self.metadata = self.load_data(metadata_file)
@@ -52,11 +64,32 @@ class DotaDataset(Dataset):
     
     
     def to_qa(self):
-        pass
+        # construct question-answer pairs from the metadata, question about the anomaly type in the video
+        qa_pairs = []
+        for video_info in self.data:
+            video_id = video_info['video_id']
+            question = f"<image>\nWhat is the anomaly type in the video?"
+            answer = video_info['anomaly_class']
+            image_path = str(video_info['video_path'])
+            qa_pairs.append(sample_format_lambda(question, answer, image_path, video_id))
+        with open(DOTA_CLEAN_DIR / "dota_qa_pairs.json", 'w') as f:
+            json.dump(qa_pairs, f, indent=4)
+        return qa_pairs
     
     def to_loc(self):
-        pass
-    
+        # question1 = f'The video\'s duration is {duration}s. Please predict the start time of the event "{data["question"]}" in this video, the event starts at'
+        loc_pairs = []
+        for video_info in self.data:
+            video_id = video_info['video_id']
+            duration = video_info['num_frames'] / video_info['fps']
+            question1 = f'The video\'s duration is {duration}s. Please predict the start time of the event "{video_info["anomaly_class"]}" in this video, the event starts at'
+            loc_pairs.append(sample_format_lambda(question1, "", "", video_id))
+        with open(DOTA_CLEAN_DIR / "dota_loc_pairs.json", 'w') as f:
+            json.dump(loc_pairs, f, indent=4)
+        return loc_pairs
+
+
+
     def get_video_frames(self, video_path, start_frame, end_frame):
         frames = []
         cap = cv2.VideoCapture(str(video_path))
