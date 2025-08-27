f218620cdb79e4fbaf53ddba9af17f7e462e51c0
FanBB2333 <2504082957@qq.com>
2025-08-27 12:59:55 +0800

    chore: 72b config

--- eval_image/vllm_qwen_72b.sh
+++ eval_image/vllm_qwen_72b.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5      # 单卡示例；多卡改成 0,1,2,3 等
+export CUDA_VISIBLE_DEVICES=1,2     # 单卡示例；多卡改成 0,1,2,3 等
 export VLLM_USE_MODELSCOPE=False   # 默认从 HuggingFace Hub；若用 ModelScope 改成 True
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-vllm serve /home7/fzy/models/Qwen/Qwen2.5-VL-72B-Instruct-AWQ \
+vllm serve ~/models/Qwen/Qwen2.5-VL-72B-Instruct-AWQ \
   --host 0.0.0.0 \
   --port 58072 \
   --max-model-len 3072 \
   --gpu-memory-utilization 0.95 \
-  --tensor-parallel-size 6 \
+  --tensor-parallel-size 2 \
   --enforce-eager \
   --enable-chunked-prefill \
-  --swap-space 16 \
+  --swap-space 8 \
   --served-model-name Qwen2.5-VL-72B-Instruct-AWQ
 
 
