a450b77374cf73f3f4ae49ec43a571e835ac24f7
FanBB2333 <2504082957@qq.com>
2025-08-28 20:30:15 +0800

    chore: sync

--- download.sh
+++ download.sh
@@ -2,3 +2,8 @@
 
 # Qwen/Qwen2.5-VL-72B-Instruct-AWQ
 hf download Qwen/Qwen2.5-VL-72B-Instruct-AWQ --local-dir Qwen2.5-VL-72B-Instruct-AWQ
+
+# nvidia/Llama-3.1-Nemotron-Nano-VL-8B-V1
+hf download nvidia/Llama-3.1-Nemotron-Nano-VL-8B-V1 --local-dir Llama-3.1-Nemotron-Nano-VL-8B-V1
+
+hfd.sh nvidia/Llama-3.1-Nemotron-Nano-VL-8B-V1 --local-dir nvidia/Llama-3.1-Nemotron-Nano-VL-8B-V1 -x 10
--- eval_image/vllm_qwen_72b.sh
+++ eval_image/vllm_qwen_72b.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=2     # 单卡示例；多卡改成 0,1,2,3 等
+export CUDA_VISIBLE_DEVICES=1     # 单卡示例；多卡改成 0,1,2,3 等
 export VLLM_USE_MODELSCOPE=False   # 默认从 HuggingFace Hub；若用 ModelScope 改成 True
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export VLLM_USE_TRITON_FLASH_ATTN=0
 
 vllm serve ~/models/Qwen/Qwen2.5-VL-72B-Instruct-AWQ \
   --host 0.0.0.0 \
   --port 58072 \
-  --max-model-len 4096 \
+  --max-model-len 8096 \
   --gpu-memory-utilization 0.95 \
   --tensor-parallel-size 1 \
+  --data-parallel-size 1 \
   --enforce-eager \
   --enable-chunked-prefill \
   --swap-space 8 \
   --served-model-name Qwen2.5-VL-72B-Instruct-AWQ
 
+# ssh -L 58072:localhost:58072 fzy@10.130.129.35 -p 1049
 
 
 
