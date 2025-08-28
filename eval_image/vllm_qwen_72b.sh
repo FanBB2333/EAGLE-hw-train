a6f612b0c7e4cca917e315024e3811f217111f97
FanBB2333 <2504082957@qq.com>
2025-08-28 10:12:26 +0800

    chore: sync

--- eval_image/vllm_qwen_72b.sh
+++ eval_image/vllm_qwen_72b.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=1,2     # 单卡示例；多卡改成 0,1,2,3 等
+export CUDA_VISIBLE_DEVICES=2     # 单卡示例；多卡改成 0,1,2,3 等
 export VLLM_USE_MODELSCOPE=False   # 默认从 HuggingFace Hub；若用 ModelScope 改成 True
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 vllm serve ~/models/Qwen/Qwen2.5-VL-72B-Instruct-AWQ \
   --host 0.0.0.0 \
   --port 58072 \
-  --max-model-len 3072 \
+  --max-model-len 4096 \
   --gpu-memory-utilization 0.95 \
-  --tensor-parallel-size 2 \
+  --tensor-parallel-size 1 \
   --enforce-eager \
   --enable-chunked-prefill \
   --swap-space 8 \
