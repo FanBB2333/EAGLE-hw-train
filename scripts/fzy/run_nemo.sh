1f68e25b560bf310d7dd017c11ed7c8da920f593
FanBB2333 <2504082957@qq.com>
2025-08-29 12:33:07 +0800

    chore: scripts

--- scripts/fzy/run_nemo.sh
+++ scripts/fzy/run_nemo.sh
@@ -11,8 +11,8 @@ python eval_image/eval_image_all.py --model_path ./checkpoints/disk2/Images/fine
 python eval_image/eval_image_all.py --model_path ./checkpoints/disk2/Images/finetune/pr_llm/finetune-image-llama3.2-3b-fzy-qwen2vl-batch-llava-eagle-ocrb-nemotron-8b-en    --gpus 0 --datasets ocrbenchv2 2>&1 | tee eval_nemo_en.log
 
 # run only_eval
-python eval_ocrbenchv2.py --model_path ./checkpoints/disk2/Images/finetune/pr_llm/finetune-image-llama3.2-3b-fzy-qwen2vl-batch-llava-eagle-ocrb-nemotron-8b-en-pr --only_eval
-python eval_ocrbenchv2.py --model_path ./checkpoints/disk2/Images/finetune/pr_llm/finetune-image-llama3.2-3b-fzy-qwen2vl-batch-llava-eagle-ocrb-nemotron-8b-en    --only_eval
+# python eval_ocrbenchv2.py --model_path ./checkpoints/disk2/Images/finetune/pr_llm/finetune-image-llama3.2-3b-fzy-qwen2vl-batch-llava-eagle-ocrb-nemotron-8b-en-pr --only_eval
+# python eval_ocrbenchv2.py --model_path ./checkpoints/disk2/Images/finetune/pr_llm/finetune-image-llama3.2-3b-fzy-qwen2vl-batch-llava-eagle-ocrb-nemotron-8b-en    --only_eval
 
 
 # scp to 9008
